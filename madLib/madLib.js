71ad04c2d15fcdc01a22dfe035f7e0c3b2becb30
halleric <42150930+halleric@users.noreply.github.com>
2019-01-14 08:53:29 -0500

    Update madLib.js

--- madLib/madLib.js
+++ madLib/madLib.js
@@ -1,1 +1,25 @@
 
+$(document).ready(function(){
+
+    $("#button").click(function(){
+
+        showInfo();
+
+    })
+
+});
+var userName;
+
+
+function getInfo(){
+    userName = $("#userName").val();
+}
+
+function showInfo(){
+    getInfo();
+    var message = "Hello" +  userName + ", glad to meet you!";
+
+    $('#ouput').text(messsage);
+    $("#output").text(message).show().animate({'top':'0px',"opacity":'1'},'slow');
+}
+

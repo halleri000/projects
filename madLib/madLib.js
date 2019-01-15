e8b1ee296540b9abee9028cc772d94a7559f4e6c
unknown <halleri000@warren.k12.in.us>
2019-01-15 13:44:05 -0500

    n

--- madLib/madLib.js
+++ madLib/madLib.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
-    $("#button").click(function(){
+    $("#submit").click(function(){
         showInfo();
     })
 });
 
+var favColor;
+var superlative;
+var adjective;
 
-function getInfoColor(){
+
+function getInfo(){
+    superlative = $('#superlative').val();
+    adjective2 = $('#adjective2').val();
+    adjective3 = $('#adjective3').val();
+    adjective4 = $('#adjective4').val();
+    adjective = $('#adjective1').val();
     favColor = $('#favColor').val();
+    bodyPart = $('#bodyPart').val();
+    noun = $('#noun').val();
+    place = $('#place').val();
+    
+    
+    
+    
 }
 function showInfo(){
 getInfo();
-var message = "your favorite color is" +  favColor;
+var message = "Your favorite color is " + favColor  + superlative + adjective + bodyPart + noun + adjective2 + adjective3 + adjective4 + place ;
 
-$('#output').text(messsage).show();
+$('#output').text(message).show();
 }

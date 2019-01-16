d9f9eca7ff8e8bbab1b3fd919d7b299c872a203a
halleric <42150930+halleric@users.noreply.github.com>
2019-01-15 22:30:21 -0500

    Update madLib.js

--- madLib/madLib.js
+++ madLib/madLib.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $("#submit").click(function(){
         showInfo();
@@ -10,23 +11,26 @@ var adjective;
 
 
 function getInfo(){
-    superlative = $('#superlative').val();
-    adjective2 = $('#adjective2').val();
-    adjective3 = $('#adjective3').val();
-    adjective4 = $('#adjective4').val();
-    adjective = $('#adjective1').val();
-    favColor = $('#favColor').val();
-    bodyPart = $('#bodyPart').val();
-    noun = $('#noun').val();
+    noun1 = $('#noun1').val();
+    noun2 = $('#noun2').val();
+    noun3 = $('#noun3').val();
+    occupation = $('#occupation').val();
+    verb = $('#verb').val();
     place = $('#place').val();
-    
+    verbEd = $('#verbEd').val();
+    noun4 = $('#noun4').val();
+    verbIng = $('#verbIng').val();
+     nounP = $('#nounP').val();
+      noun5 = $('#noun5').val();
+       emotion = $('#emotion').val();
     
     
     
 }
 function showInfo(){
 getInfo();
-var message = "Your favorite color is " + favColor  + superlative + adjective + bodyPart + noun + adjective2 + adjective3 + adjective4 + place ;
+var message = "It was during the battle of " + noun1 + " when I was running through a " + noun2 + " when a " + noun3 + " went off right next to my platoon. Our " + occupation + " yelled for us to " + verb + " to the nearest " + place + " we could find. When we got to the " + place + " we " + verbEd + " to start a fire. As we were starting the fire the enemy saw the " + noun4 + " from the fire and started "+ verbIng +  "at us. we all quickly ducked behind the " + nounP + " at the " + place + "and returned fire. we quickly eliminated the enemy and were " + emotion + " that we had won the battle."	
+
 
 $('#output').text(message).show();
 }

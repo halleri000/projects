e5a5851af1dc16cf785341bca2bec4c4ff144ac2
halleric <42150930+halleric@users.noreply.github.com>
2019-01-17 11:16:21 -0500

    Update madLib.js

--- madLib/madLib.js
+++ madLib/madLib.js
@@ -1,10 +1,14 @@
-
 $(document).ready(function () {
     $("#submit").click(function () {
         $('#forms').hide()
-        $('#back').show()
+        $('#submit').hide()
         showInfo();
     });
+   $("#back").click(function(){
+   			$("#forms").show()
+        $('#submit').show()
+        $("#output").hide()
+   });
 })
 
 var favColor;
@@ -34,6 +38,6 @@ function showInfo() {
     getInfo();
     var message = "It was during the battle of " + noun1 + " when I was running through a " + noun2 + " when a " + noun3 + " went off right next to my platoon. Our " + occupation + " yelled for us to " + verb + " to the nearest " + place + " we could find. When we got to the " + place + " we " + verbEd + " to start a fire. As we were starting the fire the enemy saw the " + noun4 + " from the fire and started " + verbIng + "at us. We all quickly ducked behind the " + nounP + " at the " + place + " and returned fire. We quickly eliminated the enemy and were " + emotion + " that we had won the battle."
 
-    $("#output").text(message).show().slideDown().animate({'top':'10px',"opacity":'1'},'slow');
+$('#back').show(); $("#output").text(message).show().slideDown().animate({'top':'10px',"opacity":'1'},'slow');
 
 }

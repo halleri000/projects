f91392256b8e9ef7f689366261f4c1ca8a79f142
unknown <halleri000@warren.k12.in.us>
2019-01-28 13:37:37 -0500

    gb

--- funMath/funMath.js
+++ funMath/funMath.js
@@ -1,58 +1,52 @@
-$("#document").ready(function(){
+$("#document").ready(function () {
 
-	$("#convert").click(function(){
-  	$("#output").toggle();
+  $("#convert").click(function () {
+    $("#output").toggle();
     add();
   });
-    $("#compute").click(function(){
-    	$("#output2").toggle();
+
+  $("#compute").click(function () {
+    $("#output2").toggle();
     moneh();
   });
-    $("#convert2").click(function(){
-      $("#output3").toggle;
-      weight();
-    })
+
+  $("#convert2").click(function () {
+    $("#output3").toggle;
+    weight();
+  });
 })
-var add = function(){
-    var year;
-	  var day;
 
-    var x = $("#minutes").val();
- year =  Math.floor( x /525600);
- var remainingMinutes=x%525600;
- day = Math.floor(remainingMinutes / 1440);
-// var remainingMinutes = x % 525600;
-//var day = remainingMinutes / 1440;
+var add = function () {
+  var year;
+  var day;
+  var x = $("#minutes").val();
+  year = Math.floor(x / 525600);
+  var remainingMinutes = x % 525600;
+  day = Math.floor(remainingMinutes / 1440);
+
   var message = "years: " + year + " days: " + day;
-    $('#output').text(message);
-
-
-}
-var moneh = function(){
-
-var x = $('#subtotal').val();
-var y = $('#gratuity').val();
-x = parseInt(x);
-y = parseInt(y);
-var gratuityamount = x * (y / 100.0); 
-var total = x  + gratuityamount;
-var message="gratuity: $" + gratuityamount + " Total: $" + total;
-
-$("#output2").text(message);
+  $('#output').text(message);
 }
 
-var weight = function(){
+var moneh = function () {
+  var x = $('#subtotal').val();
+  var y = $('#gratuity').val();
+  x = parseInt(x);
+  y = parseInt(y);
+  var gratuityamount = x * (y / 100.0);
+  var total = x + gratuityamount;
+  var message = "gratuity: $" + gratuityamount + " Total: $" + total;
+  $("#output2").text(message);
+}
 
+var weight = function () {
   var x = $("#pounds").val();
   var y = $("#inches").val();
-
-  var kg = x * 0.454;
+  var kg = x * 0.45359237;
   var m = y * 0.0254;
-  var pow = Math.pow(m,2);
-
-  var BMI = kg/pow;
-
+  var pow = Math.pow(m, 2);
+  var BMI = kg / pow;
   var message = "BMI is: " + BMI;
-
   $("#output3").text(message);
 }
+

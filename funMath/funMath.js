704a5d4d0b8ac272673829b64bece12bc59ff517
halleric <42150930+halleric@users.noreply.github.com>
2019-01-27 22:21:28 -0500

    Update funMath.js

--- funMath/funMath.js
+++ funMath/funMath.js
@@ -1,5 +1,22 @@
-$(document).ready(function(){
-    $('#convert').click(function(){
-        $("#output").show();
-});
+$("#document").ready(function(){
+
+	$("#go").click(function(){
+  	$("#output").toggle();
+    add();
+  })
 })
+var add = function(){
+    var year;
+	  var day;
+
+    var x = $("#number1").val();
+ year =  Math.floor( x /525600);
+ var remainingMinutes=x%525600;
+ day = Math.floor(remainingMinutes / 1440);
+// var remainingMinutes = x % 525600;
+//var day = remainingMinutes / 1440;
+  var message = "years: " + year + " days: " + day;
+    $('#output').text(message);
+
+
+}

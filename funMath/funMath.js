c85e49b5c3477daf21ee3cd3113ef45d161186b3
unknown <halleri000@warren.k12.in.us>
2019-01-29 13:41:15 -0500

    jnhbhv

--- funMath/funMath.js
+++ funMath/funMath.js
@@ -1,72 +1,56 @@
-$("#document").ready(function() {
+$("#document").ready(function () {
 
-  $("#convert").click(function() {
-    $("#output").toggle();
-    add();
+  $("#convert").click(function () {
+    problem1();
   });
-  $("#compute").click(function() {
-    $("#output2").toggle();
-    moneh();
+
+  $("#compute").click(function () {
+    problem2();
   });
-  $("#convert2").click(function() {
-    $("#output3").toggle();
-    weight();
+
+  $("#convert2").click(function () {
+    problem3();
   });
-  $("#convert3").click(function() {
-    $("#output4").toggle();
-    time();
+  $("#convert3").click(function () {
+    problem4();
   });
 })
-var add = function() {
+
+var problem1 = function () {
   var year;
   var day;
-
   var x = $("#minutes").val();
   year = Math.floor(x / 525600);
   var remainingMinutes = x % 525600;
   day = Math.floor(remainingMinutes / 1440);
-  // var remainingMinutes = x % 525600;
-  //var day = remainingMinutes / 1440;
-  var message = "years: " + year + " days: " + day;
-  $('#output').text(message);
-
-
+  $('#output1').toggle().text("Years: " + year + " Days: " + day);
 }
-var moneh = function() {
 
+var problem2 = function () {
   var x = $('#subtotal').val();
   var y = $('#gratuity').val();
   x = parseInt(x);
   y = parseInt(y);
-  var gratuityamount = x * (y / 100.0);
+  var gratuityamount = (y / 10.0);
   var total = x + gratuityamount;
-  var message = "gratuity: $" + gratuityamount + " Total: $" + total;
-
-  $("#output2").text(message);
+  $("#output2").toggle().text(" Gratuity: $" + gratuityamount + " Total: $" + total);
 }
 
-var weight = function() {
-
+var problem3 = function () {
   var x = $("#pounds").val();
   var y = $("#inches").val();
-
-  var kg = x * 0.454;
+  var kg = x * 0.45359237;
   var m = y * 0.0254;
   var pow = Math.pow(m, 2);
-
   var BMI = kg / pow;
-  BMI = BMI.toFixed(2);
-  var message = "BMI is: " + BMI;
-
-  $("#output3").text(message);
+  BMI = BMI.toFixed(4);
+  $("#output3").toggle().text("BMI is: " + BMI);
 }
 
-var time = function() {
-
+var problem4 = function () {
   var x = $("#decimal").val();
   var hours = Math.floor(x);
   var decimalMinutes = x % 1;
   var minutes = Math.ceil(decimalMinutes * 60);
-  $("#output4").text(hours + " Hours " + minutes + "  Minutes");
+  $("#output4").toggle().text(hours + " Hours, " + minutes + "  Minutes");
 }
-

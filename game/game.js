729d0457afad8418028e526f3d08ec5f788b0ef2
unknown <halleri000@warren.k12.in.us>
2019-03-13 13:43:06 -0400

    d

---
+++ game/game.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $("#space1").text(numbers[4]);
+    $("#space3").text(numbers[1]);
+    $("#space4").text(numbers[0]);
+    $("#space6").text(numbers[3]);
+    $("#space7").text(numbers[6]);
+
+    $("#space9").text(numbers[1]);
+    $("#space13").text(numbers[5]);
+    $("#space15").text(numbers[4]);
+
+    $("#space20").text(numbers[0]); 
+    $("#space21").text(numbers[4]);
+    $("#space22").text(numbers[2]);
+    $("#space26").text(numbers[1]);
+
+    $("#space27").text(numbers[4]);
+    $("#space28").text(numbers[7]);
+    $("#space32").text(numbers[3]);
+    $("#space33").text(numbers[8]);
+    $("#space35").text(numbers[0]);
+
+    $("#space38").text(numbers[6]);
+    $("#space39").text(numbers[7]);
+    $("#space43").text(numbers[2]);
+
+    $("#space45").text(numbers[5]);
+    $("#space50").text(numbers[4]);
+
+    $("#space55").text(numbers[3]);
+    $("#space57").text(numbers[2]);
+    $("#space59").text(numbers[0]);
+
+    $("#space65").text(numbers[4]);
+    $("#space66").text(numbers[3]);
+    $("#space70").text(numbers[1]);
+
+
+    $("#space74").text(numbers[2]);
+    $("#space75").text(numbers[8]);
+    $("#space80").text(numbers[4]);
+
+
+
+
+});
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+

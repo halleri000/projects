e73bdde1b0ec71d97cc64ca659eb776ec443bd39
unknown <halleri000@warren.k12.in.us>
2019-04-29 13:41:39 -0400

    '

--- final/final.js
+++ final/final.js
@@ -1,21 +1,37 @@
 var config = {
     type: Phaser.AUTO,
     scale: {
-             mode: Phaser.Scale.FIT,
-             autoCenter: Phaser.Scale.CENTER_BOTH,
-             width:800,
-             height:600
-         },
- physics: {
-     default: 'arcade',
-     arcade: {
-         gravity: { y: 300 },
-         debug: false
-     }
- },
- scene: {
-     preload: preload,
-     create: create,
-     update: update
- }
- };
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: init,
+        create: rendering,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+
+
+function init(){
+    this.load.image('sky', 'assets/sky.png')
+}
+
+function rendering(){
+    this.add.image(400, 300, 'sky');
+}
+
+function update(){
+
+}

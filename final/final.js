8276e3d375266440d84c63a30663f515b862027f
unknown <halleri000@warren.k12.in.us>
2019-05-02 12:58:37 -0400

    k

--- final/final.js
+++ final/final.js
@@ -29,12 +29,8 @@ var game = new Phaser.Game(config);
 
 function init() {
     this.load.image('sky', 'assets/sky.png')
-    this.load.image('1background', 'assets/BGTile (1).png')
-    this.load.image('2background', 'assets/BGTile (2).png')
-    this.load.image('3background', 'assets/BGTile (3).png')
-    this.load.image('4background', 'assets/BGTile (4).png')
     this.load.image('1tile','assets/Tile (1).png')
-    this.load.spritesheet('dude', 'assets/idle.png',{frameWidth: 250, frameHeight: 430});
+
 
 }
 
@@ -47,7 +43,8 @@ function rendering() {
   
 
     this.add.image(40,600,'1tile').setScale(.35);
-    this.add.sprite(40,30, 'dude').setScale(.20)
+    player = this.physics.add.sprite(40,30, 'dude').setScale(.20)
+    player.setGravityY(300);
 
 
 

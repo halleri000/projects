1d1707840f77fa0f3c83c4a1f0da244e628f9317
unknown <halleri000@warren.k12.in.us>
2019-05-20 13:37:02 -0400

    k

--- final/final.js
+++ final/final.js
@@ -1,101 +1,88 @@
+var config = {
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            //  gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: init,
+        create: rendering,
+        update: update
+    }
+};
 
-    var config = {
-    
-    
-        type: Phaser.AUTO,
-        scale: {
-                 mode: Phaser.Scale.FIT,
-                 autoCenter: Phaser.Scale.CENTER_BOTH,
-                 width:800,
-                 height:600
-             },
-      scale: {
-         parent: 'phaser-example',
-         mode: Phaser.Scale.FIT,
-         autoCenter: Phaser.Scale.CENTER_BOTH,
-         width: 800,
-         height: 600
-      },
- 
-     physics: {
-         default: 'arcade',
-         arcade: {
-           //  gravity: { y: 300 },
-             debug: false
-         }
-     },
-     scene: {
-         preload: init,
-         create: rendering,
-         update: update
-     }
-
-     
-     };
-
-     var game = new Phaser.Game(config);
-
- function init(){
-
-    this.load.image('pship', 'assets/sprPlayer.png')
-    this.load.image('shield', 'assets/sprShieldTile.png')
-    this.load.spritesheet('enemy1', 'assets/sprEnemy0.png', {frameWidth:8,frameHeight:8, })
-    this.load.image('boop','assets/thing.gif')
-    
+var game = new Phaser.Game(config);
+var score = 0;
+var scoreText;
+function init() {
+    this.load.image('ship', 'assets/sprPlayer.png')
+    this.load.spritesheet('enemy1', 'assets/sprEnemy0.png', { frameWidth: 8, frameHeight: 8, })
+    this.load.image('bullets', 'assets/beep.png')
 }
 
- function rendering(){
-     player = this.physics.add.image(400,500,'pship').setScale(3);
-        //player.setCollideWorldBounds(true);
+function rendering() {
+    player = this.physics.add.image(400, 500, 'ship').setScale(3);
+    //player.setCollideWorldBounds(true);
 
-        bop=this.physics.add.group();
-        
-        for(let i=2; i<10;i++){
-        for(let x=1; x<4; x++){
-          bop.create(i*75,x*75,'enemy1').setScale(4);
+    scoreText = this.add.text(16,550, 'Score: 0', {fontSize: "32px", fill:"#000"});
+
+    enemy = this.physics.add.group();
+
+    for (let i = 2; i < 10; i++) {
+        for (let j = 1; j < 4; j++) {
+            enemy.create(i * 75, j * 75, 'enemy1').setScale(4);
         }
-      
-        }
+    }
 
-       // bop=this.physics.add.sprite(100,100,'enemy1').setScale(5);
+    bullets = this.physics.add.group()
 
-
-
-
+    this.physics.add.collider(enemy, bullets)
+    this.physics.add.overlap(enemy, bullets, hitEnemy, null, this)
 
 
 
-       
-
-        
-
- 
-
-
+    function hitEnemy(enemy,bullets){
+        enemy.disableBody(true, true);
+        bullets.disableBody(true,true);
+        score += 100;
+        scoreText.setText("Score: " + score);
+    }
 
     cursors = this.input.keyboard.createCursorKeys();
 
- }
+}
 
- function update(){
+function update() {
 
+    //bop.setVelocityX(160);
+    //  if (bop.x > 950)
+    //{
+    //    bop.x = -150;
+    //}
 
-bop.setVelocityX(160);
-    if (bop.x > 950)
-    {
-        bop.x = -150;
-    }
-        
-    if(cursors.left.isDown){
+    if (cursors.left.isDown) {
         player.setVelocityX(-160);
-        //player.anims.play('left', true);
-    }else if(cursors.right.isDown){
+    }
+    else if (cursors.right.isDown) {
         player.setVelocityX(160);
-        //player.anims.play('right',true);
-    }else{
+    }
+    else {
         player.setVelocityX(0);
-        //player.anims.play('forward');
+    }
+
+    if(cursors.up.isDown){
+        bullets.create(player.x, player.y, 'bullets').setScale(0.019);
+        bullets.setVelocityY(-160);
     }
 
 
- }
+}

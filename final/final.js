729ee4d050e6039f9a580033f3e504b9b759f952
halleric <42150930+halleric@users.noreply.github.com>
2019-05-21 07:31:25 -0400

    Update final.js

--- final/final.js
+++ final/final.js
@@ -9,13 +9,6 @@
                  width:800,
                  height:600
              },
-      scale: {
-         parent: 'phaser-example',
-         mode: Phaser.Scale.FIT,
-         autoCenter: Phaser.Scale.CENTER_BOTH,
-         width: 800,
-         height: 600
-      },
  
      physics: {
          default: 'arcade',
@@ -45,26 +38,56 @@
 }
 
  function rendering(){
-     player = this.physics.add.image(400,500,'pship').setScale(3);
+    player = this.physics.add.image(400,500,'pship').setScale(3);
         //player.setCollideWorldBounds(true);
 
         bop=this.physics.add.group();
         
         for(let i=2; i<10;i++){
         for(let x=1; x<4; x++){
-          bop.create(i*75,x*75,'enemy1').setScale(4);
+         bop.create(i*75,x*75,'enemy1').setScale(4);
         }
+     }
+     //   bop = this.physics.add.sprite(100,100,'enemy1').setScale(4);
+        
+        
+        bullet = this.physics.add.group({
+       // maxSize:30,
+         //       runChildUpdate: true
+        });
+            //this.physics.add.collider(bop, bullet);
+                this.physics.add.collider(bop, bullet, hitEnemy, null, this);
+
+
+
+
+
+
+
+			function hitEnemy(bop, bullet){
+      bop.disableBody(true,true);
+
       
-        }
+       particles = this.add.particles('boomboom');
+    particles.createEmitter({
+        alpha: { start: 1, end: 0 },
+        scale: { start: .05,end:.05},
+        tint: { start: 0xff945e, end: 0xff945e },
+        speed: 50,
+        accelerationY: -50,
+        angle: { min: -85, max: -95 },
+        rotate: { min: -180, max: 180 },
+        lifespan: { min: 1000, max: 1100 },
+       // blendMode: 'ADD',
+        maxParticles: 5,
+        x: bop.x,
+        y: bop.y 
+       
+    });
 
-       // bop=this.physics.add.sprite(100,100,'enemy1').setScale(5);
-
-
-
-
-
-
-
+  
+       
+      }
        
 
         

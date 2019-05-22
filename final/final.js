68bfe2cd17b460b60dceda85323150dcc8369ff9
unknown <halleri000@warren.k12.in.us>
2019-05-22 13:41:58 -0400

    l

--- final/final.js
+++ final/final.js
@@ -33,38 +33,38 @@ function init() {
 }
 
 function rendering() {
-
-    this.anims.create({
-        key: 'enemy',
-        frames: this.anims.generateFrameNumbers('enemy1', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
     
     player = this.physics.add.image(400, 500, 'ship').setScale(3);
     player.setCollideWorldBounds(true);
 
-    scoreText = this.add.text(16,550, 'Score: 0', {fontSize: "32px", fill:"#000"});
+    scoreText = this.add.text(16,550, 'Score: 0', {fontSize: "32px", fill:'white'});
 
     enemy = this.physics.add.group();
-    for (let i = 2; i < 10; i++) {
-        for (let j = 1; j < 4; j++) {
-            enemy.create(i * 75, j * 75, 'enemy1').setScale(4);
-        }
-    }
+    //for (let row = 2; row < 10; row++) {
+      //  for (let col = 1; col < 4; col++) {
+        //    enemy.create(row * 75, col * 75, 'enemy1').setScale(4);
+       // }
+   // }
+
+   enemy = this.physics.add.image(Phaser.Math.Between(100,800), Phaser.Math.Between(100,600), 'enemy1').setScale(3);
+   enemy.setCollideWorldBounds(true);
+
+   this.physics.moveToObject(enemy, player, Phaser.Math.Between(200,1000));
+
 
     bullets = this.physics.add.group({
         //maxSize:2,
         //runChildrenUpdate: true
     });
 
+    this.physics.add.collider(enemy, player, hitPlayer);
     this.physics.add.collider(enemy, bullets, hitEnemy, null, this);
+    particles = this.add.particles('boomboom');
 
     function hitEnemy(enemy, bullets){
         enemy.disableBody(true,true);
         bullets.disableBody(true,true);
 
-    particles = this.add.particles('boomboom');
     particles.createEmitter({
         alpha: { start: 1, end: 0 },
         scale: { start: .05,end:.05},
@@ -77,20 +77,40 @@ function rendering() {
         // blendMode: 'ADD',
         maxParticles: 5,
         x: enemy.x,
-        y: enemy.y 
+        y: enemy.y,
     });
+
     }
+
+    function hitPlayer(player, enemy){
+        //this.physics.pause();
+        player.disableBody(true,true);
+        enemy.disableBody(true,true);
+    
+        particles.createEmitter({
+            alpha: { start: 1, end: 0 },
+            scale: { start: .05,end:.05},
+            tint: { start: 0xff945e, end: 0xff945e },
+            speed: 50,
+            accelerationY: -50,
+            angle: { min: -85, max: -95 },
+            rotate: { min: -180, max: 180 },
+            lifespan: { min: 1000, max: 1100 },
+            // blendMode: 'ADD',
+            maxParticles: 5,
+            x: player.x,
+            y: player.y ,
+
+    });
+}
 
     cursors = this.input.keyboard.createCursorKeys();
 
 }
 
 function update(time){
-//bop.setVelocityX(160);
-//  if (bop.x > 950)
-//{
-//  bop.x = -150;
-//}
+//enemy.setVelocity(160);
+
 
 if(cursors.left.isDown){
     player.setVelocityX(-160);
@@ -98,10 +118,16 @@ if(cursors.left.isDown){
 else if(cursors.right.isDown){
     player.setVelocityX(160);
 }
+else if(cursors.down.isDown){
+    player.setVelocityY(160);
+}
+else if(cursors.up.isDown){
+    player.setVelocityY(-160);
+}
 else{
     player.setVelocityX(0);
 }
-if(cursors.up.isDown && time > lastFired){
+if(cursors.space.isDown && time > lastFired){
     bullets.create(player.x,player.y,'bullets').setScale(.019);
     bullets.setVelocityY(-160)
     lastFired = time+50;

c67a4e49c27cda7aa2e71e89be12d6f1e7d5f614
Eric M Hall <68755975+halleri000@users.noreply.github.com>
2022-04-25 11:08:11 -0400

    Update madLib.js

--- madLib/madLib.js
+++ madLib/madLib.js
@@ -34,7 +34,7 @@ function getInfo() {
 
 function showInfo() {
         getInfo();
-            var message = "It was during the battle of " + noun1 + " when I was running through a " + noun2 + " when a " + noun3 + " went off right next to my platoon. Our " + occupation + " yelled for us to " + verb + " to the nearest " + place + " we could find. When we got to the " + place + " we " + verbEd + " to start a fire. As we were starting the fire the enemy saw the " + noun4 + " from the fire and started " + verbIng + "at us. We all quickly ducked behind the " + nounP + " at the " + place + " and returned fire. We quickly eliminated the enemy and were " + emotion + " that we had won the battle."
+            var message = "It was during the battle of " + noun1 + " when I was running through a " + noun2 + " when a " + noun3 + " went off right next to my platoon. Our " + occupation + " yelled for us to " + verb + " to the nearest " + place + " we could find. When we got to the " + place + " we " + verbEd + " to start a fire. As we were starting the fire the enemy saw the " + noun4 + " from the fire and started " + verbIng + " at us. We all quickly ducked behind the " + nounP + " at the " + place + " and returned fire. We quickly eliminated the enemy and were " + emotion + " that we had won the battle."
 
         $("#output").text(message).slideDown('slow').animate({'top':'0',"opacity":'1'},'slow');
 
